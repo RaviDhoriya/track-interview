0e35c256c9665c0f4a44d18ca4eebeb9396acb28
Ravi Dhoriya <rb.dhoriya@gmail.com>
2021-02-03 01:53:19 +0530

    React test build

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app=express();
 
+app.use(express.static(__dirname+"/public"));
+
 app.get("/api/hello",(req,res)=>{
     res.send({hello:"World"});
+});
+app.get("/",(req,res)=>{
+    res.render("index.html");
 });
 
 app.listen(process.env.PORT || 5001,()=>{
