36851da9894905ce3ec5234c4cb059cef543e221
Ravi Dhoriya <rb.dhoriya@gmail.com>
2021-02-03 23:35:02 +0530

    BUILD

--- controller/jobsController.js
+++ controller/jobsController.js
@@ -5,13 +5,23 @@ var api={};
 api.getAll=(req,res)=>{
     Jobs.find((err,data)=>{
         if(err){
-            res.send(Error(err));
+            console.error(err);
+            res.send(Error("Failed to get Jobs"));
         }else{
             res.send(Success(data));
         }
     });
 }
-
+api.getJobDetails=(req,res)=>{
+    Jobs.find({_id:req.params.job_id},(err,data)=>{
+        if(err){
+            console.error(err);
+            res.send(Error("Failed to get Job details"));
+        }else{
+            res.send(Success(data[0]));
+        }
+    });
+}
 api.jobAdd=(req,res)=>{
     var {body}=req;
     var rec=new Jobs();
@@ -34,6 +44,53 @@ api.jobAdd=(req,res)=>{
             res.send(Error("Failed to add new job"));
         }else{
             res.send(Success(data,"New job added"));
+        }
+    });
+}
+
+api.jobEdit=(req,res)=>{
+    var {body}=req;
+    
+    var job_id=body.job_id;
+    Jobs.findById(job_id,(err,data)=>{
+        if(err){
+            res.send(Error("Job post not found"));
+            console.error(err);
+        }else{
+            var rec=data;
+            rec.name=body.name;
+            rec.company=body.company;
+            rec.job_url=body.job_url;
+            rec.hr=body.hr;
+            rec.status="applied";
+            rec.source=body.source;
+            rec.notes=body.notes;
+            rec.skills=body.skills;
+            rec.ctc_min=body.ctc_min;
+            rec.ctc_max=body.ctc_max;
+            rec.location=body.location;
+            rec.applied=new Date(body.applied);
+            rec.save((err2,data2)=>{
+                if(err2){
+                    console.error(err2);
+                    res.send(Error("Failed to Edit job"));
+                }else{
+                    res.send(Success(data2,"Job updated"));
+                }
+            });
+        }
+    });
+}
+api.jobDelete=(req,res)=>{
+    var {body}=req;
+    
+    var job_id=body.job_id;
+    Jobs.deleteOne({_id:job_id},(err)=>{
+        if(err){
+            console.error(err);
+            res.send(Error("Failed to delete job"));
+        }else{
+            res.send(Success([],"Job post deleted"));
         }
     });
 }
--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -2,5 +2,8 @@ const jobs=require("../controller/jobsController");
 
 module.exports=(router)=>{
     router.get("/jobs",jobs.getAll);
+    router.get("/jobs/:job_id",jobs.getJobDetails);
     router.post("/jobs",jobs.jobAdd);
+    router.put("/jobs",jobs.jobEdit);
+    router.delete("/jobs",jobs.jobDelete);
 };
